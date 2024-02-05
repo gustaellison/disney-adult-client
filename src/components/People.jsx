519a27966af1fca965f81bf55e7000dc5a4f182f
gustaellison <gustaellison@gmail.com>
2024-02-05 10:11:53 -0500

    removing home page and adding people details component

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,13 +1,9 @@
 
-const People = ({ people }) => {
+const People = () => {
     return (
         <div>
             <h1>People</h1>
-            {people.map((person)=> (
-                <>
-                <p>{person.name}</p>
-                </>
-            ))}
+        
 
         </div>
     )
