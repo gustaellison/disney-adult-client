66016d4cb5711f3ff839ca94fce8767e09088357
HeatherRouke <heather214@gmail.com>
2024-02-05 10:18:19 -0500

    pulled changes

--- src/assets/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-export const BASE_URL = ''
-
-const Client = axios.create({baseURL: BASE_URL})
-
-export default Client
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // Wrap our App component in BrowserRouter
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
-  // Wrap our App component in BrowserRouter
+  </React.StrictMode>,
 )
