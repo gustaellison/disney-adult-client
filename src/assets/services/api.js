54483fc2a5eeb9746974a9c9b77ab20120335d26
gustaellison <gustaellison@gmail.com>
2024-02-05 11:29:41 -0500

    push

--- src/assets/services/api.js
+++ src/assets/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const BASE_URL = ''
+export const BASE_URL = 'https://disney-adults-7bc8b57c75ad.herokuapp.com'
 
 const Client = axios.create({baseURL: BASE_URL})
 
